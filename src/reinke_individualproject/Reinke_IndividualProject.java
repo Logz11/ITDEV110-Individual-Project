482a313f8c41c8f7392b36cb77b8b7b5109aa6cd
Logz11 <reinkell@gmatc.matc.edu>
2020-03-21 17:21:29 -0500

    ITDEV110 Individual Project Initial Commit

---
+++ src/reinke_individualproject/Reinke_IndividualProject.java
@@ -0,0 +1,12 @@
+package reinke_individualproject;
+
+public class Reinke_IndividualProject {
+
+    public static void main(String[] args) {
+        
+        Controller controller = new Controller();
+        controller.start();
+        
+    }
+
+}
