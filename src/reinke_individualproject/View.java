a51220f101e4b8ca4fa78f9f0c2ac1b4538f3755
Logz11 <reinkell@gmatc.matc.edu>
2020-04-06 21:50:13 -0500

    Mostly finished application, need to integrate java IO File Handling for high scores

--- src/reinke_individualproject/View.java
+++ src/reinke_individualproject/View.java
@@ -43,8 +43,7 @@ public class View {
     }
     
     public int getUserAffinity(int userCombatStyleChoice){
-        int userAffinity = 2, userConfirmationNum = 1;
-        String userConfirmationInput;
+        int userAffinity = 5;
         
             if (userCombatStyleChoice == 1) {
                 String str = JOptionPane.showInputDialog("Hero, with which form of Magic do you fight?"
@@ -60,28 +59,83 @@ public class View {
                         + "\n(1) for the Sword"
                         + "\n(2) for the Bow");
                 userAffinity = Integer.parseInt(str);
-                userConfirmationInput= JOptionPane.showInputDialog("You have selected " + userAffinityChoice
-                        +"\nIS this correct?"
+            }
+        return userAffinity;
+    }
+    
+    public int getUserConfirmation(String userAffinityChoice){
+        String userConfirmationInput = JOptionPane.showInputDialog("You have selected " + userAffinityChoice
+                        +"\nIs this correct?"
                         +"\n Enter (0) for No"
                         +"\n Enter (1) for Yes");
-                userConfirmationNum = Integer.parseInt(userConfirmationInput);
-            }
-        }
-        
-        while (userConfirmationNum != 1 );
-
-        return userAffinity;
+        int userConfirmationNum = Integer.parseInt(userConfirmationInput);
+        return userConfirmationNum;
     }
 
     public void displayASCIIArt(String ASCIIArt){
         JOptionPane.showMessageDialog(null, ASCIIArt);
     }
     
+    public void displayUserTitleAndQuarry(String creatureType, String userName, String playerCharacterTitle, String userAffinityChoice, String environment, String creatureDescription){
+        JOptionPane.showMessageDialog(null, "You are "+userName + playerCharacterTitle +", master of "+userAffinityChoice+"."+
+                "\nYou have come to "+environment+" to hunt a "+creatureType + creatureDescription);
+    }
 
-    
-    public void getCreatureType(String creatureType){
-        JOptionPane.showMessageDialog(null, creatureType);
+    public void beginCombat(String creatureType){//PLACEHOLDER NEEDS WORK
+        JOptionPane.showMessageDialog(null, "Leelde leedle leedle lee Oh no, the "+creatureType+" confronts you!");
     }
     
+    public int getUserAttackOrDefend(){
+        String str = JOptionPane.showInputDialog("Will you attack the monster or defend yourself?"
+                + "\nEnter (0) to defend yourself"
+                + "\nEnter (1) to attack the monster");
+        return Integer.parseInt(str);
+    }
     
+    public void combatTurnResults(int turnNumber, String flavorText){
+        JOptionPane.showMessageDialog(null, "~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ Turn " + turnNumber + " ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n"+flavorText);
+    }
+    
+    public void displayHealthTotals(double monsterHealth, double playerHealth){
+        JOptionPane.showMessageDialog(null, "Monster Health: " + monsterHealth
+            + "\nPlayer Health: " +playerHealth);
+    }
+    
+    public void combatOutcomeMessage(double monsterHealth, double playerHealth, int turnCount){
+        //PLAYER VICTORY
+        if (playerHealth > 0.00){
+            JOptionPane.showMessageDialog(null, "congration you're winner."
+                    + "\nYou defeated the monster in "+turnCount+" turns"
+                    + "\nwith "+playerHealth+" lifepoints remaining.");
+        }
+        //PLAYER DEFEAT
+        else {
+            JOptionPane.showMessageDialog(null, "You have been defeated!"
+                    +"\nThough you fought valiantly"
+                    +"\nand reduced the monster's  lifepoints to "+monsterHealth
+                    +"\nyou had no lifepoints remaining.");
+        }
+    }
+    
+    public void displayUserScore(double userScore){
+        JOptionPane.showMessageDialog(null, "Your score for this hunt is "+userScore);
+    }
+    
+    public void displayHighScores(){
+        
+    }
+    
+    public int userPlayAgain(){
+        String str = JOptionPane.showInputDialog("Would you like to embark on another hunt?"
+            +"\n Enter (0) for No"
+            +"\n Enter (1) for Yes");
+        int userPlayAgain = Integer.parseInt(str);
+        return userPlayAgain;
+    }
+    
+    public void exitMessage(String userName, String playerCharacterTitle){
+        JOptionPane.showMessageDialog(null,"So ends the song of " + userName + playerCharacterTitle
+            +"\nAnd you close the Book of Heroes.");
+    }
+
 }
